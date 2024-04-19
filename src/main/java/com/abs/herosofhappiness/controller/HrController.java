1c38a396c1ce24195439340a59c159f3c0c01c00
rohansahu <sahurohankumar47@gmail.com>
2024-04-19 23:10:31 +0530

    changes in .properties file

--- src/main/java/com/abs/herosofhappiness/controller/HrController.java
+++ src/main/java/com/abs/herosofhappiness/controller/HrController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -17,6 +18,7 @@ import com.abs.herosofhappiness.service.HrService;
 
 @RestController
 @RequestMapping("hr")
+@CrossOrigin("http://localhost:3000")
 public class HrController {
 	
 	@Autowired
